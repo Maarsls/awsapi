a9d844b95ffd79502ead7870ae62d38581e5a818
Maarsls <maffey.marcel@gmx.net>
2022-05-17 22:04:14 +0200

    changed authentication

--- routes/shopify.js
+++ routes/shopify.js
@@ -6,7 +6,7 @@ const secretKey = process.env.SHOPIFYKEY;
 const seatsio = require("seatsio");
 const getRawBody = require("raw-body");
 
-router.post("/webhooks/orders/create", async (req, res) => {
+router.post("/webhooks/orders/createw", async (req, res) => {
   console.log(req)
   console.log("🎉 We got an order!");
   console.log(secretKey);
@@ -51,5 +51,41 @@ router.post("/webhooks/orders/create", async (req, res) => {
     res.sendStatus(403);
   }
 });
+
+
+router.post("/webhooks/orders/create", async (req, res) => {
+  console.log("Webhook heard!");
+  // Verify
+  const hmac = req.header("X-Shopify-Hmac-Sha256");
+  const topic = req.header("X-Shopify-Topic");
+  const shop = req.header("X-Shopify-Shop-Domain");
+
+  const verified = verifyWebhook(req.body, hmac);
+
+  if (!verified) {
+    console.log("Failed to verify the incoming request.");
+    res.status(401).send("Could not verify request.");
+    return;
+  }
+
+  const data = req.body.toString();
+  const payload = JSON.parse(data);
+  console.log(
+    `Verified webhook request. Shop: ${shop} Topic: ${topic} \n Payload: \n ${data}`
+  );
+
+  res.status(200).send("OK");
+});
+
+// Verify incoming webhook.
+function verifyWebhook(payload, hmac) {
+  const message = payload.toString();
+  const genHash = crypto
+    .createHmac("sha256", process.env.SHOPIFYKEY)
+    .update(message)
+    .digest("base64");
+  console.log(genHash);
+  return genHash === hmac;
+}
 
 module.exports = router;
