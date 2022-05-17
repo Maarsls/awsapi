cb40557180398022af4fcfe791ae0bace3870600
Maarsls <maffey.marcel@gmx.net>
2022-05-17 23:37:07 +0200

    final

--- webhookserver.js
+++ webhookserver.js
@@ -1,54 +1,57 @@
-const express = require('express')
-const app = express()
-const getRawBody = require('raw-body')
-const crypto = require('crypto')
-const secretKey = process.env.SHOPIFYKEY
+const express = require("express");
+const app = express();
+const getRawBody = require("raw-body");
+const crypto = require("crypto");
+const secretKey = process.env.SHOPIFYKEY;
 const seatsio = require("seatsio");
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+  res.send("Hello World!");
+});
 
-app.post('/shopify/webhooks/orders/create', async (req, res) => {
-  console.log('🎉 We got an order!')
+app.post("/shopify/webhooks/orders/create", async (req, res) => {
+  console.log("🎉 We got an order!");
 
   // We'll compare the hmac to our own hash
-  const hmac = req.get('X-Shopify-Hmac-Sha256')
+  const hmac = req.get("X-Shopify-Hmac-Sha256");
 
   // Use raw-body to get the body (buffer)
-  const body = await getRawBody(req)
+  const body = await getRawBody(req);
 
   // Create a hash using the body and our key
   const hash = crypto
-    .createHmac('sha256', secretKey)
-    .update(body, 'utf8', 'hex')
-    .digest('base64')
+    .createHmac("sha256", secretKey)
+    .update(body, "utf8", "hex")
+    .digest("base64");
 
   // Compare our hash to Shopify's hash
   if (hash === hmac) {
     // It's a match! All good
-    console.log('Phew, it came from Shopify!')
+    console.log("Phew, it came from Shopify!");
     console.log("job done");
     const order = JSON.parse(body.toString());
-    let seatsArray = [];
-    let orderNote = order.note;
-    const noWhitespace = orderNote.replace(/\s/g, "");
-    seatsArray = noWhitespace.split(",");
-    seatsArray.pop();
-    console.log(seatsArray);
-    // It's a match! All good
-    let client = new seatsio.SeatsioClient(
-      seatsio.Region.EU(),
-      process.env.SEATSIOKEY
-    );
-    await client.events.book("agiball2022", seatsArray);
-    res.sendStatus(200)
+    if (order.note) {
+      let seatsArray = [];
+      let orderNote = order.note;
+      const noWhitespace = orderNote.replace(/\s/g, "");
+      seatsArray = noWhitespace.split(",");
+      seatsArray.pop();
+      console.log(seatsArray);
+      // It's a match! All good
+      let client = new seatsio.SeatsioClient(
+        seatsio.Region.EU(),
+        process.env.SEATSIOKEY
+      );
+      await client.events.book("agiball2022", seatsArray);
+    }
+    console.log("Phew, it came from Shopify!");
+    res.sendStatus(200);
   } else {
     // No match! This request didn't originate from Shopify
-    console.log('Danger! Not from Shopify!')
-    res.sendStatus(403)
+    console.log("Danger! Not from Shopify!");
+    res.sendStatus(403);
   }
-})
+});
 // Dependencies
 const fs = require("fs");
 const http = require("http");
@@ -76,5 +79,5 @@ const credentials = {
 // Starting both http & https servers
 const httpsServer = https.createServer(credentials, app);
 httpsServer.listen(443, () => {
-    console.log("HTTPS Server running on port 443");
-  });
+  console.log("HTTPS Server running on port 443");
+});
