30bb1f9a0b1601cd18d76f9a9d25e7fca97265c4
Maarsls <maffey.marcel@gmx.net>
2022-05-17 14:38:25 +0200

    added https

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-const http = require("http");
 const app = require("./app");
-const server = http.createServer(app);
-
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
-const auth = require("./middleware/auth");
+// Dependencies
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 
-// server listening
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+// Certificate
+const privateKey = fs.readFileSync(
+  process.cwd() + "/etc/letsencrypt/live/tyvent.at/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  process.cwd() + "/etc/letsencrypt/live/tyvent.at/cert.pem",
+  "utf8"
+);
+const ca = fs.readFileSync(
+  process.cwd() + "/etc/letsencrypt/live/tyvent.at/chain.pem",
+  "utf8"
+);
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(5001, () => {
+  console.log("HTTP Server running on port 5001");
+});
+
+httpsServer.listen(5000, () => {
+  console.log("HTTPS Server running on port 5000");
 });
