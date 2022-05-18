a037b33e77734794fda9864d007b6f39a55e3a4f
Maarsls <maffey.marcel@gmx.net>
2022-05-18 18:40:29 +0200

    j

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ httpServer.listen(5001, () => {
   console.log("HTTP Server running on port 5001");
 });
 
-httpsServer.listen(443, () => {
+httpsServer.listen(5000, () => {
   console.log("HTTPS Server running on port 443");
 });
