3a9ee4af8066f13f466a8ef18f58e9bf57dce8dd
maarsls <maffey.marcel@gmx.net>
2022-08-08 12:14:17 +0200

    adddedevent

--- model/events.js
+++ model/events.js
@@ -3,6 +3,8 @@ mongoose.set("debug", true);
 
 const blogSchema = new mongoose.Schema({
   event: { type: String },
+  key: { iv: { type: String }, encryptedData: { type: String } },
+
 });
 
 module.exports = mongoose.model("events", blogSchema, "events");
